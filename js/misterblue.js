7e00f0e668e10c1e6c938d7188a160533bdc269c
Robert Adams <misterblue@misterblue.com>
2016-09-05 22:30:33 -0700

    Add JS so hash anchors aren't hidden by the top menu.

--- js/misterblue.js
+++ js/misterblue.js
@@ -4,3 +4,9 @@
 $(document).click(function() {
     $(".blindtoggle").toggle("blind");
 });
+
+// Fix for hash tags getting covered by the top menu bar
+// Ref: https://github.com/twbs/bootstrap/issues/1768
+var shiftWindow = function() { scrollBy(0, -50); };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
